38630aa5760c3813713e7feb7787781a9044ce23
Noa <noasalgadodev@gmail.com>
2022-07-02 21:41:46 +0200

    Added exercise from lesson 5

---
+++ 05-Números/index.js
@@ -0,0 +1,17 @@
+//Una variable que contenga tu altura en centímetros (entero)
+let alturaCm = 168;
+
+//Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaM = 1.68;
+
+//Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = 60.5;
+
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaCeil = Math.ceil(alturaM);
+
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoFloor = Math.floor(peso);
+
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+esIgual = Number.MAX_VALUE + 1 === Number.MAX_VALUE; //true
